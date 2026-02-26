7b86938bf3887ee2c4e654542c57150e63f9ce17
Nagul75 <nagulk697@gmail.com>
2026-02-26 15:54:14 +0530

    CS-4 add auth router and helmet

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
+import authRouter from "./routes/authRouter";
+import cookieParser from "cookie-parser";
+import "./types/auth";
 import morgan from "morgan";
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
+app.use(cookieParser());
 app.use(morgan("dev"));
 
-app.get("/", (_req, res) => {
-  res.json({ message: "API running" });
-});
+app.use("/auth", authRouter);
 
 export default app;
