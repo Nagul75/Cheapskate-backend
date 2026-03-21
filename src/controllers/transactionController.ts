3875cdb82ad7dd17b8da549a91efd1d6865e5b11
Nagul75 <nagulk697@gmail.com>
2026-03-21 10:54:24 +0530

    CS-18 remove unused import

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 
 import prisma from "../db/prisma";
 import { TransactionType } from "../generated/prisma/enums";
-import { Prisma } from "../generated/prisma/client";
 
 async function getUserTransactions(req: Request, res: Response) {
   const user = req.user;
